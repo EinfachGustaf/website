95fd16cf77d2513ffb0ba2bd9e321d556ca300a0
Simoso68 <87707341+Simoso68@users.noreply.github.com>
2023-08-03 10:38:34 +0200

    changed copy field and title text

--- js/copy-effect.js
+++ js/copy-effect.js
@@ -2,10 +2,10 @@ function copy_effect() {
     var copybtn = document.getElementById("copy-field");
     copybtn.animate(
         [
-          { background: '#494949'},
+          { background: '#525252'},
           { background: '#222121'}
         ], {
-          duration: 200,
+          duration: 300,
           iterations: 1
         }
       );
